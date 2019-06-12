32d32a8ee8d12b602b264d53aaaa8b558f0de0a8
rekaram <51747472+rekaram@users.noreply.github.com>
2019-06-12 18:30:24 +0530

    Add files via upload

--- guvi2.c
+++ guvi2.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main(){
-long long int num;
-scanf("%d",&num);
-if(num%2==0){
-printf("Even");}
-else if(num%2!=0)
-    printf("Odd");
-else{
-printf("Invalid");
-}}
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c){
+        printf("%d",a);
+    }
+else if(b>a && b>c){
+        printf("%d",b);
+    }
+  else{
+    printf("%d",c);
+  }
+return 0;
+}
